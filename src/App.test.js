ab625503b02d551d1152dbc012684415e10a5900
Gavril Hasmasan <gavriel.hasmasan@global-fashion-group.com>
2022-06-29 13:17:48 +0300

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,19 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<App/>);
+  const linkElement = screen.getByText('Test');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App/>);
+  const linkElement = screen.getByText('Test');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App/>);
+  const linkElement = screen.getByText('Test');
   expect(linkElement).toBeInTheDocument();
 });
